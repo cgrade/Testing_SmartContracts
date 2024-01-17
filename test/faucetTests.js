ad628153725ce9353f08fbcd590ec158781066ab
cgrade <gradeisageek@gmail.com>
2024-01-17 13:54:28 +0100

    Unit test updated

--- test/faucetTests.js
+++ test/faucetTests.js
@@ -1,19 +1,19 @@
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { expect } = require('chai');
-const hre = require('hardhat');
 
 describe('Faucet', function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
   async function deployContractAndSetVariables() {
-    const Faucet = await hre.ethers.getContractFactory('Faucet');
+    const Faucet = await ethers.getContractFactory('Faucet');
     const faucet = await Faucet.deploy();
 
-    const [owner] = await hre.ethers.getSigners();
+    const [owner] = await ethers.getSigners();
 
-    console.log('Signer 1 address: ', owner.address);
-    return {faucet, owner};
+    let withdrawAmount = ethers.utils.parseUnits('0.0000000000001', 'ether');
+
+    return { faucet, owner, withdrawAmount };
   }
 
   it('should deploy and set the owner correctly', async function () {
@@ -22,4 +22,10 @@ describe('Faucet', function () {
     expect(await faucet.owner()).to.equal(owner.address);
   });
 
+  it('should not allow withdrawals above .1 ETH at a time', async function () {
+    const { faucet, withdrawAmount } = await loadFixture(
+      deployContractAndSetVariables
+    );
+    await expect(faucet.withdraw(withdrawAmount)).to.be.reverted;
+  });
 });
